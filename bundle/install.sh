31443c6f0a287b534c8a2862882e91de2a21115f
baixinchen <baixinchen@tencent.com>
2017-04-01 17:46:29 +0800

    更新

--- bundle/install.sh
+++ bundle/install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
+git clone https://github.com/kern/vim-es7.git
 git clone https://github.com/jiangmiao/auto-pairs
 git clone https://github.com/haya14busa/incsearch.vim
-git clone https://github.com/pangloss/vim-javascript
 git clone https://github.com/jistr/vim-nerdtree-tabs
 git clone https://github.com/kien/ctrlp.vim
 git clone https://github.com/scrooloose/nerdtree
-git clone https://github.com/isruslan/vim-es6
 git clone https://github.com/elzr/vim-json
-git clone https://github.com/anyakichi/vim-surround
 git clone https://github.com/mattn/emmet-vim
 git clone https://github.com/bling/vim-airline
 git clone https://github.com/tpope/vim-fugitive
